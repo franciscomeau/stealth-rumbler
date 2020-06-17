d04db6226500c41faa4556a3fb3209994df75b62
franciscomeau <francis.comeau@mail.mcgill.ca>
2020-06-17 14:36:01 -0400

    implemented step listener and vibrate
    didnt test it yet

--- StealthRumbler/app/src/main/java/com/example/stealthrumbler/ConnectToFriendActivity.java
+++ StealthRumbler/app/src/main/java/com/example/stealthrumbler/ConnectToFriendActivity.java
@@ -2,13 +2,45 @@ package com.example.stealthrumbler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobScheduler;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 
-public class ConnectToFriendActivity extends AppCompatActivity {
+
+public class ConnectToFriendActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private JobScheduler job;
+    private Vibrator v;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_to_friend);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        v.vibrate(500);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
