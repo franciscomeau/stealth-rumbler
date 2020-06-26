64303783849b4a8c504ee3ad7ca33e54023675a4
franciscomeau <francis.comeau@mail.mcgill.ca>
2020-06-26 01:41:34 -0400

    use accelerometer instead of step detector
    step detector not available on my test phone

--- StealthRumbler/app/src/main/java/com/example/stealthrumbler/ConnectToFriendActivity.java
+++ StealthRumbler/app/src/main/java/com/example/stealthrumbler/ConnectToFriendActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.job.JobScheduler;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.widget.Toast;
+
+import static android.content.Context.VIBRATOR_SERVICE;
 
 
 public class ConnectToFriendActivity extends AppCompatActivity implements SensorEventListener {
@@ -19,6 +23,9 @@ public class ConnectToFriendActivity extends AppCompatActivity implements Sensor
     private Sensor sensor;
     private JobScheduler job;
     private Vibrator v;
+    double ax,ay,az;   // these are the acceleration in x,y and z axis
+    double sensitivity = 9.;
+
 
 
 
@@ -27,17 +34,36 @@ public class ConnectToFriendActivity extends AppCompatActivity implements Sensor
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_to_friend);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        PackageManager pm = this.getPackageManager();
+        if(pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_ACCELEROMETER)) {
+            Toast.makeText(this, "Has sensor accelerometer!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "No sensor accelerometer!", Toast.LENGTH_LONG).show();
+        }
 
-        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
     }
 
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        v.vibrate(500);
+        //v.vibrate(500);
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            ax=event.values[0];
+            ay=event.values[1];
+            az=event.values[2];
+
+
+            if (Math.max(Math.max(ax,ay),az)>=sensitivity) {
+                v.vibrate(500);
+            }
+        }
     }
 
     @Override
